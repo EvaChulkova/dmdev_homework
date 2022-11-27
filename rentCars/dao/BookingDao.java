5969dfb37bce0792347131f040a37aabfa546831
Evgeniya <89831421+EvaChulkova@users.noreply.github.com>
2022-11-28 00:54:11 +0300

    RENT CAR PROJECT

--- rentCars/dao/BookingDao.java
+++ rentCars/dao/BookingDao.java
@@ -5,7 +5,12 @@ import rentCars.entity.Booking;
 import rentCars.exception.RentCarsDaoException;
 import rentCars.util.RentCarsConnectionManager;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
